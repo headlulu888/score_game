e7a6d57389fe84675f4747c5d410e1928f49faca
headlulu888 <headlulu888@gmail.com>
2019-05-18 13:59:12 +0300

    create box

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var $start = document.querySelector('#start')
 var $game = document.querySelector('#game')
 
+var score = 0
+
 $start.addEventListener('click', startGame)
+$game.addEventListener('click', handleBoxClick)
 
 function startGame() {
     $game.style.backgroundColor = '#fff'
@@ -11,5 +14,23 @@ function startGame() {
 }
 
 function renderBox() {
+    $game.innerHTML = ''
+    var box = document.createElement('div')
 
+    box.style.height = box.style.width = '50px'
+    box.style.position = 'absolute'
+    box.style.backgroundColor = '#000'
+    box.style.top = '50px'
+    box.style.left = '70px'
+    box.style.cursor = 'pointer'
+    box.setAttribute('data-box', 'true')
+
+    $game.insertAdjacentElement('afterbegin', box)
+}
+
+function handleBoxClick(event) {
+    if(event.target.dataset.box) {
+        score++
+        renderBox()
+    }
 }
